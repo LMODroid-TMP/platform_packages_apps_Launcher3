fd3198264d12ee1738295451c8a84f7b28547b48
Pranav Vashi <neobuddy89@gmail.com>
2022-08-04 00:15:07 +0530

    Launcher3: Remove blur radius option if not supported
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: Ic6a1a3fd7ea6fc21094c7f4880507571f360e68b
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- src/com/android/launcher3/settings/SettingsMisc.java
+++ src/com/android/launcher3/settings/SettingsMisc.java
@@ -60,6 +60,8 @@ import com.android.launcher3.util.DisplayController;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
+import com.android.systemui.shared.system.BlurUtils;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -274,6 +276,8 @@ public class SettingsMisc extends CollapsingToolbarBaseActivity
                             }
                     );
                     return !info.isTablet(info.realBounds);
+                case Utilities.KEY_BLUR_DEPTH:
+                    return BlurUtils.supportsBlursOnWindows();
             }
 
             return true;
