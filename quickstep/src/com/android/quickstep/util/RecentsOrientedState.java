91578cb6055b9ff82be63b43771f6a8d37b8791e
SahilSonar <sss.sonar2003@gmail.com>
2023-12-10 04:37:47 +0530

    Revert "Launcher3: Add resource for home screen rotation default"
    This reverts commit 856e2642bdb5413b2f451ce3eb2bb324ee4d6b64.
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Change-Id: Id9bf637e8cb8124e8c4b14ad9d08ecd40bc0d705
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- quickstep/src/com/android/quickstep/util/RecentsOrientedState.java
+++ quickstep/src/com/android/quickstep/util/RecentsOrientedState.java
@@ -46,7 +46,6 @@ import androidx.annotation.NonNull;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.InvariantDeviceProfile;
 import com.android.launcher3.LauncherPrefs;
-import com.android.launcher3.R;
 import com.android.launcher3.testing.shared.TestProtocol;
 import com.android.launcher3.touch.PagedOrientationHandler;
 import com.android.launcher3.util.DisplayController;
@@ -294,8 +293,7 @@ public class RecentsOrientedState implements
     }
 
     private void updateHomeRotationSetting() {
-        boolean homeRotationEnabled = LauncherPrefs.get(mContext).get(ALLOW_ROTATION,
-                mContext.getResources().getBoolean(R.bool.allow_rotation));
+        boolean homeRotationEnabled = LauncherPrefs.get(mContext).get(ALLOW_ROTATION);
         setFlag(FLAG_HOME_ROTATION_ALLOWED_IN_PREFS, homeRotationEnabled);
         SystemUiProxy.INSTANCE.get(mContext).setHomeRotationEnabled(homeRotationEnabled);
     }
