5ed841ce6ab889c456859d464169ff0dd5c1883d
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-02-03 23:24:05 +0530

    Launcher3: Improve restart logic
    - Schedule restart only after launcher enters normal state otherwise
      we get a binder freeze when home animation is running
    - Use System.exit() instead of killProcess()
    - Use main executor
    
    Change-Id: Ibc27b412153110eada13b850b7dfcf32d00a0f2b

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1298,6 +1298,10 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         // Set screen title for Talkback
         setTitle(state.getTitle());
+
+        if (state == LauncherState.NORMAL) {
+            LauncherAppState.getInstance(this).checkIfRestartNeeded();
+        }
     }
 
     /**
@@ -1317,8 +1321,6 @@ public class Launcher extends StatefulActivity<LauncherState>
         } else {
             mOverlayManager.onActivityResumed();
         }
-                        
-        LauncherAppState.getInstance(this).checkIfRestartNeeded();
 
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection();
